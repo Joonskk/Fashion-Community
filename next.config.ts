ded58825ea54fe48a18cbbcc7ac0c1329777c297
JS <apple@Joons-MacBook-Air.local>
2025-07-01 18:19:15 +0900

    Fix: Replaced domains to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**", // 모든 경로 허용
+      }
+    ]
   },
   // 다른 설정들...
 };
