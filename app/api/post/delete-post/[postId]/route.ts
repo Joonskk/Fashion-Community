8551f2e65255600df4e24d76cdbef28cd6a0efdd
JS <apple@Joons-MacBook-Air.local>
2025-07-23 18:30:50 +0900

    Feat: Add Post Delete Functionality

---
+++ app/api/post/delete-post/[postId]/route.ts
@@ -0,0 +1,24 @@
+import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+// DELETE: 게시물 삭제
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function DELETE(req: NextRequest, context: any) {
+    const postId = context.params.postId;
+
+    try {
+        const db = (await clientPromise).db('wearly');
+        await db.collection("posts").deleteOne({
+            _id: new ObjectId(postId),
+        })
+
+        return NextResponse.json(
+            { message: "게시물이 성공적으로 삭제되었습니다." },
+            { status: 200 }
+        );
+    } catch(err) {
+        console.error("게시물 삭제 오류:", err);
+        return NextResponse.json({ message: "서버 오류" }, { status: 500 });
+    }
+}
--- app/post/[postId]/PostMenu.tsx
+++ app/post/[postId]/PostMenu.tsx
@@ -1,3 +1,5 @@
+import { useParams, useRouter } from "next/navigation";
+
 type PostMenuProps = {
     showPostMenu: boolean,
     setShowPostMenu: React.Dispatch<React.SetStateAction<boolean>>;
@@ -6,16 +8,35 @@ type PostMenuProps = {
 
 const PostMenu = ({showPostMenu, setShowPostMenu, copyURL} : PostMenuProps) => {
 
+    const router = useRouter();
+
+    const params = useParams();
+    const postId = params?.postId as string;
+
     const togglePostMenu = () => {
         setShowPostMenu((prev)=>!prev);
     }
 
-    const sharePost = () => {
+    const deletePost = async () => {
+        try {
+            const res = await fetch(`/api/post/delete-post/${postId}`, {
+                method: "DELETE",
+            });
 
-    }
+            if (!res.ok) {
+                throw new Error('게시물 삭제 실패');
+            }
 
-    const deletePost = () => {
+            if (res.ok){
+                console.log('게시물 삭제 성공');
+                router.push('/mypage');
+            }
 
+        } catch(err) {
+            console.error('게시물 삭제 중 오류 발생:', err);
+        } finally {
+            router.refresh();
+        }
     }
 
     return (
@@ -30,7 +51,7 @@ const PostMenu = ({showPostMenu, setShowPostMenu, copyURL} : PostMenuProps) => {
             />
             {/* 메뉴창 */}
             <div
-                className={`relative bg-white rounded-t-xl w-full max-w-[750px] h-[33%] py-[10px] transform transition-transform duration-300 ${
+                className={`relative bg-white rounded-t-xl w-full max-w-[750px] h-[180px] py-[10px] transform transition-transform duration-300 ${
                 showPostMenu ? "translate-y-0" : "translate-y-full"
                 } pointer-events-auto`}
             >
@@ -39,12 +60,6 @@ const PostMenu = ({showPostMenu, setShowPostMenu, copyURL} : PostMenuProps) => {
                     onClick={()=>{togglePostMenu(); copyURL()}}
                 >
                     링크 복사
-                </div>
-                <div
-                    className="w-full h-[80px] flex justify-center items-center hover:bg-gray-100 cursor-pointer text-[18px] text-black"
-                    onClick={()=>sharePost()}
-                >
-                    공유
                 </div>
                 <div 
                     className="w-full h-[80px] flex justify-center items-center hover:bg-gray-100 cursor-pointer text-[18px] text-red-500"
